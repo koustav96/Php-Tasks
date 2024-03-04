323d7db739e55d4e5420836d634e378c15211f63
koustav96 <koustav.mondal@innoraft.com>
2024-03-04 23:17:56 +0530

    FT2-40: Added Task4 folder containing php assignment-4.

---
+++ Task4/Form.php
@@ -0,0 +1,17 @@
+<?php
+require 'print_data.php';
+// Check if the form is submitted.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Process the form data.
+    $full_name = $_POST["f_name"] . ' ' . $_POST["l_name"];
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    $phoneNumber = $_POST["phone_number"];
+    $marksText = $_POST["subject_marks"];
+    move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+    $image = $target_file;
+    /**
+     * Print data.
+     */
+    show_data($full_name, $phoneNumber, $image, $marksText);
+}
