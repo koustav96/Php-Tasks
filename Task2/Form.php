793e2ddbcc25b831f334e79656d8d97ddcd67e05
koustav96 <koustav.mondal@innoraft.com>
2024-03-04 23:15:13 +0530

    FT2-40: Added Task2 folder containing php assignment-2.

---
+++ Task2/Form.php
@@ -0,0 +1,15 @@
+<?php
+require 'print_data.php';
+// Check if the form is submitted.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Process the form data.
+    $full_name = $_POST["f_name"] . ' ' . $_POST["l_name"];
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+    $image = $target_file;
+    /**
+     * Print data.
+     */
+    show_data($full_name, $image, $marksText);
+}
